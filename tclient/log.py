243eb280f75a0da8a44529e098c420a70b465600
huangtao <huangtao_usst@163.com>
2018-01-11 01:16:12 +0800

    完成 LogFormatter 类

--- tclient/log.py
+++ tclient/log.py
@@ -6,10 +6,10 @@
 """为 tclient 提供日志支持
 
 tclient 使用三种 logger：
-
 - tclient.monitor: 记录系统监控部分的日志和重大变更.
-- tclient.scheduler: 记录排程的运行情况
-- tclient.job: 记录排程中的每个 Job 的运行情况.要注意的是 每个 Job 可能都有自己单独的日志文件"""
+- tclient.scheduler: 记录排程的运行情况.
+- tclient.job: 记录排程中的每个 Job 的运行情况.要注意的是 每个 Job 可能都有自己单独的日志文件i.
+"""
 
 
 import logging
@@ -26,6 +26,7 @@ class LogFormatter(logging.Formatter):
 
     继承自 logging.Formatter"""
     DEFAULT_FORMAT = '[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s'
-    DEFAULT_FORMAT = '%Y-%m-%d %H:%M:%S'
+    DEFAULT_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
-    def __init__(self,):
+    def __init__(self, fmt=DEFAULT_FORMAT, datefmt=DEFAULT_DATE_FORMAT):
+        logging.Formatter.__init__(self, fmt=fmt, datefmt=datefmt)
