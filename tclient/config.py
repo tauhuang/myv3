48ea65a287d449c03436cc0dd146a1886841f94f
huangtaoa <huangtaoa@digiwin.com>
2018-01-11 17:51:11 +0800

    add log end

---
+++ tclient/config.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2018 Digiwin Inc.
+#
+
+
+root_dir = ''
--- tclient/log.py
+++ tclient/log.py
@@ -6,22 +6,19 @@
 """为 tclient 提供日志支持
 
 tclient 使用三种 logger：
-- tclient.monitor: 记录系统监控部分的日志和重大变更.
-- tclient.scheduler: 记录排程的运行情况.
-- tclient.job: 记录排程中的每个 Job 的运行情况.要注意的是 每个 Job 可能都有自己单独的日志文件i.
+- monitor: 记录系统监控部分的日志和重大变更.
+- scheduler: 记录排程的运行情况.
+- job: 记录排程中的每个 Job 的运行情况.要注意的是 每个 Job 可能都有自己单独的日志文件i.
 """
 
 
 import logging
-import logging.handlers
+import logging.handlers as handlers
+import os.path
+from tclient.config import root_dir
 
 
-mon_log = logging.getLogger('tclient.monitor')
-sched_log = logging.getLogger('tclient.scheduler')
-app_log = logging.getLogger('tclient.job')
-
-
-class LogFormatter(logging.Formatter):
+class DefaultFormatter(logging.Formatter):
     """tclient 中的日志格式化器.
 
     继承自 logging.Formatter"""
@@ -30,3 +27,43 @@ class LogFormatter(logging.Formatter):
 
     def __init__(self, fmt=DEFAULT_FORMAT, datefmt=DEFAULT_DATE_FORMAT):
         logging.Formatter.__init__(self, fmt=fmt, datefmt=datefmt)
+
+
+class DefaultHandler(handlers.TimedRotatingFileHandler):
+    """tclient 中的日志 handler
+
+    必须传入 文件名
+    """
+
+    DEFAULT_WHEN = 'd'
+    DEFAULT_COUNT = 14
+    DEFAULT_INTERVAL = 1
+
+    def __init__(self,
+                 filename,
+                 when=DEFAULT_WHEN,
+                 interval=DEFAULT_INTERVAL,
+                 backup_count=DEFAULT_COUNT,
+                 encoding='utf-8'):
+        handlers.TimedRotatingFileHandler.__init__(self,
+                                                   filename,
+                                                   when=when,
+                                                   interval=interval,
+                                                   backupCount=backup_count,
+                                                   encoding=encoding)
+        self.setLevel(logging.DEBUG)
+        self.setFormatter(DefaultFormatter())
+
+
+def construct_logger(name):
+    """传入名称，返回 logger"""
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(DefaultHandler(os.path.join(root_dir, name, name)))
+    return logger
+
+
+mon_log = construct_logger('monitor')
+sched_log = construct_logger('scheduler')
+job_log = construct_logger('job')
