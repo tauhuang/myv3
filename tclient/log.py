559a38ff458d4017af50b5bac50b0b17d702c7a1
huangtaoa <huangtaoa@digiwin.com>
2018-01-12 12:09:03 +0800

    新增 safe_logmsg 函数
    safe_logmsg 防止非 ASCII 的字符串出现编码错误

--- tclient/log.py
+++ tclient/log.py
@@ -8,7 +8,14 @@
 tclient 使用三种 logger：
 - monitor: 记录系统监控部分的日志和重大变更.
 - scheduler: 记录排程的运行情况.
-- job: 记录排程中的每个 Job 的运行情况.要注意的是 每个 Job 可能都有自己单独的日志文件i.
+- job: 记录排程中的每个 Job 的运行情况.要注意的是 每个 Job 可能都有自己单独的日志文件.
+
+每次调用 logger 的 debug, warning, error 等日志信息接口时，都要先调用 safe_logmsg(),
+以防止非 ASCII 字符串出现编码错误.
+例子：
+from tclient.log import mon_log
+
+mon_log.debug(safe_logmsg('时间 %s' % '不确定')
 """
 
 
@@ -16,6 +23,7 @@ import logging
 import logging.handlers as handlers
 import os
 from tclient.config import root_dir
+from tclient.util import to_unicode
 
 
 class DefaultFormatter(logging.Formatter):
@@ -81,3 +89,7 @@ def construct_logger(name):
 mon_log = construct_logger('monitor')
 sched_log = construct_logger('scheduler')
 job_log = construct_logger('job')
+
+
+def safe_logmsg(msg):
+    return to_unicode(msg)
