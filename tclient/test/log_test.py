62f109fbd549638d04ee9d7dc72321f0ab1a09ba
huangtao <huangtao_usst@163.com>
2018-01-24 01:48:17 +0800

    new test

--- tclient/test/log_test.py
+++ tclient/test/log_test.py
@@ -2,10 +2,14 @@
 
 
 from loadpackage import *
-from tclient.log import mon_log, sched_log, job_log, safe_logmsg
+from tclient.log import job_log, safe_logmsg
+
+
+def write_log():
+    job_log.debug(safe_logmsg('时间'))
+    job_log.error(safe_logmsg('错误 %s' % '编码错误'))
+    job_log.critical(safe_logmsg('fatal： {0}'.format('系统崩溃')))
 
 
 if __name__ == '__main__':
-    mon_log.debug(safe_logmsg('时间'))
-    sched_log.error(safe_logmsg('错误 %s' % '编码错误'))
-    job_log.critical(safe_logmsg('fatal： {0}'.format('系统崩溃')))
+    write_log()
