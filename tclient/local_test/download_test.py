7551c919f0eb1aade48ff96d3bf2915e4d111a65
huangtaoa <huangtaoa@digiwin.com>
2018-01-30 18:38:24 +0800

    download test

--- tclient/local_test/download_test.py
+++ tclient/local_test/download_test.py
@@ -2,5 +2,5 @@ from loadpackage import *
 from tclient.download import get_download_url, download_zip
 
 
-print get_download_url('2.2.2', '')
+print get_download_url('2.2.2', 'TABVAAV')
 download_zip()
