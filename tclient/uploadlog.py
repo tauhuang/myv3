26f2d0aec91cee5672d1637c88fa2a597a36ebd8
huangtaoa <huangtaoa@digiwin.com>
2018-01-31 18:46:26 +0800

    refact uploadlog(not ending)

--- tclient/uploadlog.py
+++ tclient/uploadlog.py
@@ -8,12 +8,77 @@ import datetime
 import os
 import requests
 import uuid
-from config import safe_baseurl, erp_license
+from config import safe_baseurl, erp_license, ROOT_DIR
 from tclient.log import job_log, DATE_FORMAT, BASE_LOGDIR
-from tclient.util import cal_file_md5
+from tclient.util import cal_file_md5, mkdir_not_exists, compress_file
 
 
 _LOG_ID = uuid.uuid4()
+
+
+class LogData(object):
+    def __init__(self, erplicense=erp_license):
+        self._erplic = erplicense
+        self._md5 = None
+        self._filename = None
+        self._logdate = None
+        self._base64string = None
+
+    def _construct(self, attr_name):
+        if hasattr(self, attr_name) and not (self.__dict__[attr_name] is None):
+            return self.__dict__[attr_name]
+        else:
+            return ''
+
+    @property
+    def erplicense(self):
+        return self._erplic
+
+    @property
+    def md5(self):
+        return self._construct('_md5')
+
+    @md5.setter
+    def md5(self, md5_str):
+        self._md5 = md5_str
+
+    @property
+    def filename(self):
+        return self._construct('_filename')
+
+    @filename.setter
+    def filename(self, file_name):
+        self._filename = file_name
+
+    @property
+    def logdate(self):
+        return self._construct('_logdate')
+
+    @logdate.setter
+    def logdate(self, logdate_str):
+        # format: 20180105
+        self._logdate = repr(logdate_str)
+
+    @property
+    def base64string(self):
+        return self._construct('_base64string')
+
+    @base64string.setter
+    def base64string(self, base64_str):
+        self._base64string =  base64_str
+
+    @property
+    def json(self):
+        return {
+            "erpLic": self.erplicense,
+            "md5": self.md5,
+            "fileName": self.filename,
+            "logDate": self.logdate,
+            "base64String": self.base64string
+        }
+
+
+_logdata = LogData()
 
 
 def return_log_path():
@@ -27,6 +92,22 @@ def return_files():
     return [os.path.join(log_path, i) for i in os.listdir(log_path)]
 
 
+def return_compressed_files():
+    files = return_files()
+    tempdir = os.path.join(ROOT_DIR, 'tmp')
+    mkdir_not_exists(tempdir)
+    compressed_files = []
+    for f in files:
+        try:
+            compressed_filename = os.path.join(tempdir, os.path.basename(f)+'.gz')
+            compress_file(f, compressed_filename)
+            compressed_files.append(compressed_filename)
+        except Exception:
+            job_log.exception('id: {0}, Failed to compress log {1}'.format(_LOG_ID, f))
+
+    return compressed_files
+
+
 def get_filemd5(filename):
     try:
         return cal_file_md5(filename)
@@ -35,15 +116,11 @@ def get_filemd5(filename):
         return ''
 
 
-def response_ok(response, filename):
+def response_ok(response, retry_no):
     if not response.ok:
-        job_log.warning('id: {0}, request: {1}, response: code: {2}, reason: {3}, message {4}'.format(
-            _LOG_ID, response.url, response.status_code, response.reason, repr(response.content)))
+        job_log.warning('id: {0}, Retry No: {1}, request: {2}, response: code: {3}, reason: {4}, message {5}'.format(
+            _LOG_ID, retry_no, response.url, response.status_code, response.reason, repr(response.content)))
         return False
-    status = response.json()['status']
-    if status == u'notmatched':
-        job_log.warning('id: {0}, the md5 key of file "{1}" not matched between client and server'.format(
-                        _LOG_ID, filename))
     return True
 
 
@@ -52,40 +129,13 @@ def http_post(json_dict):
     retry_times = 3
     # if failed retry three times
     while retry_times:
-        retry_times -= 1
         r = requests.post(url=url, json=json_dict)
-        if response_ok(r, json_dict['fileName']):
+        if response_ok(r, 3-retry_times):
             break
+        retry_times -= 1
 
 
-def upload_file(filename):
-    post_json = {
-                 'erpLic': erp_license,
-                 'md5': get_filemd5(filename),
-                 'isEnd': 'N',
-                 'fileName': os.path.basename(filename),
-                 'logDate': os.path.basename(return_log_path())
-                 }
-    if post_json['md5']:
-        try:
-            with open(filename, 'rb') as f:
-                while True:
-                    chunk = f.read(1024*1024*10)  # 10M each time
-                    if chunk:
-                        post_json['base64String'] = base64.b64encode(chunk)
-                        http_post(post_json)
-                    else:
-                        post_json['base64String'] = ''
-                        post_json['isEnd'] = 'Y'
-                        http_post(post_json)
-                        break
-        except IOError:
-            job_log.error('id: {0}, failed to read log file {0}'.format(_LOG_ID, filename))
-        except Exception:
-            job_log.exception('id: {0}, failed to upload log file {0}'.format(_LOG_ID, filename))
-
-
-def main():
-    files = return_files()  # files is a list
-    for f in files:
-        upload_file(f)
+def upload():
+    file_list = return_compressed_files()
+    for f in file_list:
+        pass
