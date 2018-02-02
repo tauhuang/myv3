e3d4175fd11d72a261f07a3c2c922e9f70fa4fa6
huangtaoa <huangtaoa@digiwin.com>
2018-02-02 10:58:45 +0800

    更改 ROOT_DIR 的值

--- tclient/config.py
+++ tclient/config.py
@@ -3,11 +3,13 @@
 #
 
 
-import re
 import os.path
+import re
+import sys
 
 
-ROOT_DIR = ''
+# root dir 是 config.py 所在路径的父目录
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
 BASE_URL = 'http://172.16.101.43/wstopprd/ws/r'
 DEFAULT_CONF = os.path.join(ROOT_DIR, 'conf', 'tclient.cnf')
 
