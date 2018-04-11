90819e4b860614582f638acc09dcf9518905ae44
huangtaoa <huangtaoa@digiwin.com>
2018-04-11 11:33:45 +0800

    delete class SafeBaseURL

--- tclient/config.py
+++ tclient/config.py
@@ -14,27 +14,6 @@ BASE_URL = 'http://172.16.101.43/wstopprd/ws/r'
 DEFAULT_CONF = os.path.join(ROOT_DIR, 'conf', 'tclient.cnf')
 
 
-class SafeBaseURL(object):
-    def __init__(self):
-        self._base_url = BASE_URL
-
-    @property
-    def base_url(self):
-        url = self._base_url.lstrip('http://')
-        url = url.replace('//', '/')
-        url = ''.join(['http://', url])
-        return url.rstrip('/')
-
-    @base_url.setter
-    def base_url(self, value):
-        if not isinstance(value, basestring):
-            raise TypeError('base_url expected a string, you gave a {0}'.format(type(value)))
-        self._base_url = value
-
-
-safe_baseurl = SafeBaseURL()
-
-
 class ERPLicense(object):
     """T100 ERP License Number"""
 
