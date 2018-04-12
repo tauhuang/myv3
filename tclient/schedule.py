60836a8b5767137550663227f86f7e99edd95441
huangtao <huangtao_usst@163.com>
2018-04-13 05:02:36 +0800

    finish schedule

--- tclient/schedule.py
+++ tclient/schedule.py
@@ -1,18 +1,31 @@
 # -*- coding: UTF-8 -*-
 
 
-import schedule
+import time
+from schedule import Scheduler
 from random import randint
-from tclient.download import download
-from tclient.installupdate import
+from tclient.downupdate import install
+from tclient.uploadlog import upload
+from job.send import send
 
 
 def set_sched_time():
     # random time
-    random_hour = randint(0, 23)
+    random_hour = randint(0, 8)
     random_minute = randint(0, 59)
     return ":".join(str(random_hour), str(random_minute))
 
 
 def set_scheuler():
-    pass
+    sched = Scheduler()
+    sched.every().day.at(set_sched_time()).do(upload)
+    sched.every().day.at(set_sched_time()).do(install)
+    sched.every().sunday.at(set_sched_time()).do(send)
+    return sched
+
+
+def main():
+    scheduler = set_scheuler()
+    while True:
+        scheduler.run_pending()
+        time.sleep(1)
